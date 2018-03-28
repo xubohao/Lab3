8cdc5408ebab197691f05505b7ba2fe3ccb7640f
Bohao Xu <xubohao@udel.edu>
2018-03-28 22:36:54 +0800

    complete

--- FlamingoBLL/src/main/java/pkgCore/HandPoker.java
+++ FlamingoBLL/src/main/java/pkgCore/HandPoker.java
@@ -22,7 +22,7 @@ public class HandPoker extends Hand {
 
 	@Override
 	public HandScore ScoreHand() {
-		// TODO : Implement this method... call each of the 'is' methods (isRoyalFlush,
+		// DONE : Implement this method... call each of the 'is' methods (isRoyalFlush,
 		// etc) until
 		// one of the hands is true, then score the hand
 
@@ -33,6 +33,22 @@ public class HandPoker extends Hand {
 
 		} else if (isStraightFlush()) {
 
+		} else if (isFourOfAKind()) {
+		    
+		} else if (isFullHouse()) {
+		    
+		} else if (isFlush()) {
+		    
+		} else if (isStraight()) {
+		    
+		} else if (isThreeOfAKind()) {
+		    
+		} else if (isTwoPair()) {
+		    
+		} else if (isPair()) {
+		    
+		} else if (isHighCard()) {
+		    
 		}
 
 		return null;
@@ -40,25 +56,138 @@ public class HandPoker extends Hand {
 
 	public boolean isRoyalFlush() {
 		boolean bIsRoyalFlush = false;
-		// TODO : Implement this method
+        // DONE : Implement this method
+        
+        // flush?
+        boolean bisFlush = false;
+        int iCardCnt = super.getCards().size();
+        int iSuitCnt = 0;
+
+        for (eSuit eSuit : eSuit.values()) {
+            for (Card c : super.getCards()) {
+                if (eSuit == c.geteSuit()) {
+                    iSuitCnt++;
+                }
+            }
+            if (iSuitCnt > 0)
+                break;
+        }
+
+        if (iSuitCnt == iCardCnt)
+            bisFlush = true;
+        else
+            bisFlush = false;
+        
+        if (bisFlush) {
+            // straight?
+            if (this.getCRC().size() == 5) {
+                int iMaxCardNrb = this.getCRC().get(0).geteRank().getiRankNbr();
+                if (iMaxCardNrb != eRank.ACE.getiRankNbr())
+                    return false; // Not Royal~
+                int iMinCardNrb = this.getCRC().get(4).geteRank().getiRankNbr();
+                if (iMinCardNrb == iMaxCardNrb - 4)
+                {
+                    HandScorePoker HSP = (HandScorePoker) this.getHS();
+                    HSP.seteHandStrength(eHandStrength.RoyalFlush);
+                    int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+                    HSP.setHiCard(this.getCards().get(iGetCardHi));
+                    HSP.setLoCard(null);
+                    HSP.setKickers(FindTheKickers(this.getCRC()));
+                    this.setHS(HSP);
+                    bIsRoyalFlush = true;
+                }
+            }
+        }
 		return bIsRoyalFlush;
 	}
 
 	public boolean isStraightFlush() {
 		boolean bisStraightFlush = false;
-		// TODO : Implement this method
+		// DONE : Implement this method
+		
+		// flush?
+		boolean bisFlush = false;
+		int iCardCnt = super.getCards().size();
+        int iSuitCnt = 0;
+
+        for (eSuit eSuit : eSuit.values()) {
+            for (Card c : super.getCards()) {
+                if (eSuit == c.geteSuit()) {
+                    iSuitCnt++;
+                }
+            }
+            if (iSuitCnt > 0)
+                break;
+        }
+
+        if (iSuitCnt == iCardCnt)
+            bisFlush = true;
+        else
+            bisFlush = false;
+        
+        if (bisFlush) {
+            // straight?
+            if (this.getCRC().size() == 5) {
+                int iMaxCardNrb = this.getCRC().get(0).geteRank().getiRankNbr();
+                if (iMaxCardNrb == eRank.ACE.getiRankNbr())
+                    return false; // Royal Flush or Nothing~
+                int iMinCardNrb = this.getCRC().get(4).geteRank().getiRankNbr();
+                if (iMinCardNrb == iMaxCardNrb - 4)
+                {
+                    HandScorePoker HSP = (HandScorePoker) this.getHS();
+                    HSP.seteHandStrength(eHandStrength.StraightFlush);
+                    int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+                    HSP.setHiCard(this.getCards().get(iGetCardHi));
+                    HSP.setLoCard(null);
+                    HSP.setKickers(FindTheKickers(this.getCRC()));
+                    this.setHS(HSP);
+                    bisStraightFlush = true;
+                }
+            }
+        }
+		
 		return bisStraightFlush;
 	}
 	
-	// TODO : Implement this method
+	// DONE : Implement this method
 	public boolean isFourOfAKind() {
 		boolean bisFourOfAKind = false;
+        if (this.getCRC().size() == 2) {
+            if (this.getCRC().get(0).getiCnt() == Constants.FOUR_OF_A_KIND) {
+                if (this.getCRC().get(1).getiCnt() == Constants.ONE_OF_A_KIND) {
+                    HandScorePoker HSP = (HandScorePoker) this.getHS();
+                    HSP.seteHandStrength(eHandStrength.FourOfAKind);
+                    int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+                    HSP.setHiCard(this.getCards().get(iGetCardHi));
+                    int iGetCardLo = this.getCRC().get(1).getiCardPosition();
+                    HSP.setLoCard(this.getCards().get(iGetCardLo));
+                    HSP.setKickers(FindTheKickers(this.getCRC()));
+                    this.setHS(HSP);
+                    bisFourOfAKind = true;
+                }
+            }
+        }
 		return bisFourOfAKind;
 	}
 
-	// TODO : Implement this method
+	// DONE : Implement this method
 	public boolean isFullHouse() {
 		boolean bisFullHouse = false;
+        if (this.getCRC().size() == 2) {
+            if (this.getCRC().get(0).getiCnt() == Constants.THREE_OF_A_KIND) {
+                if (this.getCRC().get(1).getiCnt() == Constants.TWO_OF_A_KIND) {
+                    HandScorePoker HSP = (HandScorePoker) this.getHS();
+                    HSP.seteHandStrength(eHandStrength.FullHouse);
+                    int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+                    HSP.setHiCard(this.getCards().get(iGetCardHi));
+                    int iGetCardLo = this.getCRC().get(1).getiCardPosition();
+                    HSP.setLoCard(this.getCards().get(iGetCardLo));
+                    HSP.setKickers(FindTheKickers(this.getCRC()));
+                    this.setHS(HSP);
+                    bisFullHouse = true;
+                }
+            }
+        }
 		return bisFullHouse;
 
 	}
@@ -79,8 +208,17 @@ public class HandPoker extends Hand {
 				break;
 		}
 
-		if (iSuitCnt == iCardCnt)
-			bisFlush = true;
+		// Note: to make the methods consistent, I add these.
+		if (iSuitCnt == iCardCnt) {
+            HandScorePoker HSP = (HandScorePoker) this.getHS();
+            HSP.seteHandStrength(eHandStrength.Flush);
+            int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+            HSP.setHiCard(this.getCards().get(iGetCardHi));
+            HSP.setLoCard(null);
+            HSP.setKickers(FindTheKickers(this.getCRC()));
+            this.setHS(HSP);
+		    bisFlush = true;
+		}
 		else
 			bisFlush = false;
 
@@ -89,7 +227,22 @@ public class HandPoker extends Hand {
 
 	public boolean isStraight() {
 		boolean bisStraight = false;
-		// TODO : Implement this method
+		// DONE : Implement this method
+        if (this.getCRC().size() == 5) {
+            int iMaxCardNrb = this.getCRC().get(0).geteRank().getiRankNbr();
+            int iMinCardNrb = this.getCRC().get(4).geteRank().getiRankNbr();
+            if (iMinCardNrb == iMaxCardNrb - 4)
+            {
+                HandScorePoker HSP = (HandScorePoker) this.getHS();
+                HSP.seteHandStrength(eHandStrength.Straight);
+                int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+                HSP.setHiCard(this.getCards().get(iGetCardHi));
+                HSP.setLoCard(null);
+                HSP.setKickers(FindTheKickers(this.getCRC()));
+                this.setHS(HSP);
+                bisStraight = true;
+            }
+        }
 		return bisStraight;
 	}
 
@@ -106,6 +259,8 @@ public class HandPoker extends Hand {
 				HSP.setLoCard(null);
 				HSP.setKickers(FindTheKickers(this.getCRC()));
 				this.setHS(HSP);
+				// Note: I think this line should be added!!!
+				bisThreeOfAKind = true;
 			}
 		}
 		return bisThreeOfAKind;
@@ -113,19 +268,56 @@ public class HandPoker extends Hand {
 
 	public boolean isTwoPair() {
 		boolean bisTwoPair = false;
-		// TODO : Implement this method
+        // DONE : Implement this method
+        if (this.getCRC().size() == 3) {
+            if (this.getCRC().get(0).getiCnt() == Constants.TWO_OF_A_KIND) {
+                if (this.getCRC().get(1).getiCnt() == Constants.TWO_OF_A_KIND) {
+                    HandScorePoker HSP = (HandScorePoker) this.getHS();
+                    HSP.seteHandStrength(eHandStrength.TwoPair);
+                    int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+                    HSP.setHiCard(this.getCards().get(iGetCardHi));
+                    int iGetCardLo = this.getCRC().get(1).getiCardPosition();
+                    HSP.setLoCard(this.getCards().get(iGetCardLo));
+                    HSP.setKickers(FindTheKickers(this.getCRC()));
+                    this.setHS(HSP);
+                    bisTwoPair = true;
+                }
+            }
+        }
 		return bisTwoPair;
 	}
 
 	public boolean isPair() {
 		boolean bisPair = false;
-		// TODO : Implement this method
+        // DONE : Implement this method
+        if (this.getCRC().size() == 4) {
+            if (this.getCRC().get(0).getiCnt() == Constants.TWO_OF_A_KIND) {
+                HandScorePoker HSP = (HandScorePoker) this.getHS();
+                HSP.seteHandStrength(eHandStrength.Pair);
+                int iGetCard = this.getCRC().get(0).getiCardPosition();
+                HSP.setHiCard(this.getCards().get(iGetCard));
+                HSP.setLoCard(null);
+                HSP.setKickers(FindTheKickers(this.getCRC()));
+                this.setHS(HSP);
+                bisPair = true;
+            }
+        }
 		return bisPair;
 	}
 
 	public boolean isHighCard() {
 		boolean bisHighCard = false;
-		// TODO : Implement this method
+        // DONE : Implement this method
+        if (this.getCRC().size() == 5) {
+            HandScorePoker HSP = (HandScorePoker) this.getHS();
+            HSP.seteHandStrength(eHandStrength.HighCard);
+            int iGetCardHi = this.getCRC().get(0).getiCardPosition();
+            HSP.setHiCard(this.getCards().get(iGetCardHi));
+            HSP.setLoCard(null);
+            HSP.setKickers(FindTheKickers(this.getCRC()));
+            this.setHS(HSP);
+            bisHighCard = true;
+        }
 		return bisHighCard;
 	}
 
--- FlamingoBLL/src/test/java/pkgCore/HandPokerTest.java
+++ FlamingoBLL/src/test/java/pkgCore/HandPokerTest.java
@@ -8,64 +8,354 @@ import pkgEnum.eRank;
 import pkgEnum.eSuit;
 
 public class HandPokerTest {
+    
+    // Royal Flush: 1 test
+    @Test
+    public void RoyalFlushTest1() {
+        System.out.println("Royal Flush:");
+        System.out.println("10,J,Q,K,A");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TEN));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.JACK));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.QUEEN));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.KING));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.ACE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isRoyalFlush(), true);
+        
+        System.out.println("");
+    }
+    
+    // Royal Flush: negative test
+    @Test
+    public void RoyalFlushNegTest1() {
+        System.out.println("Fake Royal Flush:");
+        System.out.println("9,10,J,Q,K");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.NINE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TEN));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.JACK));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.QUEEN));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.KING));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isRoyalFlush(), false);
+        
+        System.out.println("");
+    }
+    
+    // Straight Flush: 1 test
+    @Test
+    public void StraightFlushTest1() {
+        System.out.println("Straight Flush:");
+        System.out.println("9,10,J,Q,K");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.NINE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TEN));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.JACK));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.QUEEN));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.KING));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isStraightFlush(), true);
+        
+        System.out.println("");
+    }
+    // Four Of a Kind: 2 tests
+    @Test
+    public void FourOfAKindTest1() {
+        System.out.println("Four of a Kind");
+        System.out.println("KKKKA");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.KING));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.KING));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.KING));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.KING));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.ACE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isFourOfAKind(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void FourOfAKindTest2() {
+        System.out.println("Four of a Kind");
+        System.out.println("JQQQQ");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.JACK));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.QUEEN));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.QUEEN));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.QUEEN));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.QUEEN));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isFourOfAKind(), true);
+        
+        System.out.println("");
+    }
+    
+    // Full house: 2 tests
+    @Test
+    public void FullHouseTest1() {
+        System.out.println("Full House");
+        System.out.println("22555");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isFullHouse(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void FullHouseTest2() {
+        System.out.println("Full house");
+        System.out.println("KKKAA");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.KING));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.KING));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.KING));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.ACE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.ACE));
+        hp.ScoreHand();
 
-	@Test
-	public void FourOfAKindTest1() {
-		System.out.println("Four of a Kind");
-		System.out.println("");
-		HandPoker hp = new HandPoker();
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.DIAMONDS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.SPADES,eRank.TWO));
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
-		hp.ScoreHand();
-		System.out.println("");
-	}
-	
-	@Test
-	public void ThreeOfAKindTest1() {
-		System.out.println("Three of a Kind");
-		System.out.println("");
-		HandPoker hp = new HandPoker();
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.DIAMONDS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
-		hp.ScoreHand();
-		System.out.println("");
-	}
-	
-	@Test
-	public void TwoPairTest1() {
-		System.out.println("Two Pair");
-		System.out.println("");
-		HandPoker hp = new HandPoker();
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FIVE));
-		hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
-		hp.ScoreHand();
-		System.out.println("");
-	}
-	
-	@Test
-	public void FullHouseTest1() {
-		System.out.println("Full House");
-		System.out.println("");
-		HandPoker hp = new HandPoker();
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
-		hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FIVE));
-		hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
-		hp.AddCard(new Card(eSuit.CLUBS,eRank.FIVE));
-		hp.ScoreHand();
-		
-		assertEquals(hp.isFullHouse(),true);
-		
-		System.out.println("");
-	}
+        assertEquals(hp.isFullHouse(), true);
+        
+        System.out.println("");
+    }
+    
+    // Flush: 1 test
+    @Test
+    public void FlushTest1() {
+        System.out.println("Flush");
+        System.out.println("C2 C3 C4 C6 C7");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.SIX));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.SEVEN));
+        hp.ScoreHand();
 
+        assertEquals(hp.isFlush(), true);
+        
+        System.out.println("");
+    }
+    
+    // Straight: 1 test
+    @Test
+    public void StraightTest1() {
+        System.out.println("Straight");
+        System.out.println("C2 C3 S4 C5 C6");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.SIX));
+        hp.ScoreHand();
+
+        assertEquals(hp.isStraight(), true);
+        
+        System.out.println("");
+    }
+    
+    // Three of a kind: 3 tests
+    @Test
+    public void ThreeOfAKindTest1() {
+        System.out.println("Three of a Kind");
+        System.out.println("22253");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isThreeOfAKind(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void ThreeOfAKindTest2() {
+        System.out.println("Three of a Kind");
+        System.out.println("23335");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isThreeOfAKind(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void ThreeOfAKindTest3() {
+        System.out.println("Three of a Kind");
+        System.out.println("23555");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isThreeOfAKind(), true);
+        
+        System.out.println("");
+    }
+	
+    // Two pairs: 3 tests
+    @Test
+    public void TwoPairTest1() {
+        System.out.println("Two Pairs");
+        System.out.println("23355");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isTwoPair(), true);
+        
+        System.out.println("");
+    }
+
+    @Test
+    public void TwoPairTest2() {
+        System.out.println("Two Pairs");
+        System.out.println("22355");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isTwoPair(), true);
+        
+        System.out.println("");
+    }
+
+    @Test
+    public void TwoPairTest3() {
+        System.out.println("Two Pairs");
+        System.out.println("22335");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isTwoPair(), true);
+        
+        System.out.println("");
+    }
+   
+    // One pair: 4 tests
+    @Test
+    public void OnePairTest1() {
+        System.out.println("One Pair");
+        System.out.println("22345");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isPair(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void OnePairTest2() {
+        System.out.println("One Pair");
+        System.out.println("23345");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isPair(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void OnePairTest3() {
+        System.out.println("One Pair");
+        System.out.println("23445");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isPair(), true);
+        
+        System.out.println("");
+    }
+    
+    @Test
+    public void OnePairTest4() {
+        System.out.println("One Pair");
+        System.out.println("23455");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.THREE));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.FIVE));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isPair(), true);
+        
+        System.out.println("");
+    }
+
+    // High card: 1 test
+    @Test
+    public void HighCardTest4() {
+        System.out.println("High Card");
+        System.out.println("24568");
+        HandPoker hp = new HandPoker();
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.TWO));
+        hp.AddCard(new Card(eSuit.CLUBS,eRank.FOUR));
+        hp.AddCard(new Card(eSuit.DIAMONDS,eRank.FIVE));
+        hp.AddCard(new Card(eSuit.HEARTS,eRank.SEVEN));
+        hp.AddCard(new Card(eSuit.SPADES,eRank.EIGHT));
+        hp.ScoreHand();
+        
+        assertEquals(hp.isHighCard(), true);
+        
+        System.out.println("");
+    }
 }
